32d858373fbb7179eb9731ef3d584e931eb1e7f3
Andre <traderdre@outlook.com>
2024-04-01 14:26:07 +0100

    Date element added

--- Check/Screens/Challenge18.js
+++ Check/Screens/Challenge18.js
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import {Text, View} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-
+import { Fontisto } from '@expo/vector-icons';
 import Challenge25 from './Challenge25 '
 import PagerView from 'react-native-pager-view'
 
+
+let day = date.getDate()
+let month = date.getMonth () + 1
+let year = date.getFullYear() - 18
+
+let fullDate = `${day}-${month}-${year}`
 
 
 
@@ -12,28 +17,31 @@ const Challenge18 = () => {
     return (
 
         <View style={{ flex: 1, backgroundColor: 'indigo' }}>
-  <PagerView style={{flex: 1}} initialPage={0}>
-            <View style={{ marginTop: 110 }} key = "1">
-                <Text style={{ color: 'white', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>The current date for Challenge 18 is:</Text>
+            <PagerView style={{ flex: 1 }} initialPage={0}>
+                <View style={{ marginTop: 110 }} key="1">
+                    <Text style={{ color: 'white', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>The current date for Challenge 18 is:</Text>
 
-                <View style = {{marginTop: 70, alignItems: 'center', flexDirection: 'column'}}>
-                <FontAwesome name="circle-thin" size={390} color="mediumspringgreen" />
 
-                <View style = {{marginTop: 50}}>
+                    <View style={{ marginTop: 150, alignItems: 'center', flexDirection: 'row', gap: 15, justifyContent: 'center', marginBottom: 50 }}>
 
-                <Text style={{ color: 'white', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>Swipe left for Challenge 25</Text>
+                        <Fontisto name="date" size={40} color="mediumspringgreen" style={{ alignContent: 'flex-start', alignItems: 'flex-start', justifyContent: 'flex-start' }} />
+                        <Text style={{ color: 'white', fontSize: 30 }}> {fullDate} </Text>
+                    </View>
+
+                    <View style={{ marginTop: 100, alignItems: 'center', flexDirection: 'column' }}>
+                        <Text style={{ color: 'white', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>Swipe left for Challenge 25</Text>
+
+
+                        <View style={{ marginTop: 20 }}>
+                            <Ionicons name="arrow-back-circle-outline" size={78} color="mediumspringgreen" />
+                        </View>
+
+                    </View>
+
                 </View>
-
-<View style = {{marginTop: 20}}>
-<Ionicons name="arrow-back-circle-outline" size={78} color="mediumspringgreen" />
-</View>
-
+                <View >
+                    <Challenge25></Challenge25>
                 </View>
-
-            </View>
-            <View >
-                <Challenge25></Challenge25>
-            </View>
 
             </PagerView>
         </View>
