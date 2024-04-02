de7407ce7a6272cfff5960afbce66375229ddade
Andre <traderdre@outlook.com>
2024-04-02 12:14:20 +0100

    Designs finalized

--- Check/Screens/Challenge18.js
+++ Check/Screens/Challenge18.js
@@ -1,14 +1,16 @@
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { Fontisto } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Challenge25 from './Challenge25 '
 import PagerView from 'react-native-pager-view'
 
 
-let day = date.getDate()
-let month = date.getMonth () + 1
-let year = date.getFullYear() - 18
+date = new Date()
 
+let day = date.getDate()
+let month = date.getMonth() + 1
+let year = date.getFullYear() - 18
 let fullDate = `${day}-${month}-${year}`
 
 
@@ -17,21 +19,25 @@ const Challenge18 = () => {
     return (
 
         <View style={{ flex: 1, backgroundColor: 'indigo' }}>
+
             <PagerView style={{ flex: 1 }} initialPage={0}>
-                <View style={{ marginTop: 110 }} key="1">
-                    <Text style={{ color: 'white', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>The current date for Challenge 18 is:</Text>
+
+                <View key="1">
+                    <View style={{ marginTop: 75, alignItems: 'center' }}>
+                        <MaterialCommunityIcons name="account-multiple-check-outline" size={50} color="white" />
+                        <Text style={{ color: 'mediumspringgreen' }}>_____________________________________________________</Text>
+                    </View>
+
+                    <Text style={{ color: 'white', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18, marginTop: 75 }}>The current date for Challenge 18 is:</Text>
 
 
                     <View style={{ marginTop: 150, alignItems: 'center', flexDirection: 'row', gap: 15, justifyContent: 'center', marginBottom: 50 }}>
-
                         <Fontisto name="date" size={40} color="mediumspringgreen" style={{ alignContent: 'flex-start', alignItems: 'flex-start', justifyContent: 'flex-start' }} />
                         <Text style={{ color: 'white', fontSize: 30 }}> {fullDate} </Text>
                     </View>
 
-                    <View style={{ marginTop: 100, alignItems: 'center', flexDirection: 'column' }}>
+                    <View style={{ marginTop: 110, alignItems: 'center', flexDirection: 'column' }}>
                         <Text style={{ color: 'white', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>Swipe left for Challenge 25</Text>
-
-
                         <View style={{ marginTop: 20 }}>
                             <Ionicons name="arrow-back-circle-outline" size={78} color="mediumspringgreen" />
                         </View>
--- Check/Screens/Challenge25 .js
+++ Check/Screens/Challenge25 .js
@@ -1,6 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import PagerView from 'react-native-pager-view'
+
+
+
+date = new Date()
+
+let day = date.getDate()
+let month = date.getMonth() + 1
+let year = date.getFullYear() - 25
+let fullDate = `${day}-${month}-${year}`
 
 
 
@@ -8,28 +19,36 @@ const Challenge25 = () => {
     return (
 
         <View style={{ flex: 1, backgroundColor: 'mediumspringgreen' }}>
-            <View style={{ marginTop: 110, alignItems: 'center'}}>
-                <Text style={{ color: 'black', textAlign: 'center', fontWeight: 'bold', fontSize: 18 }}>The current date for Challenge 25 is:</Text>
 
-                <View style = {{marginTop: 70, alignItems: 'center', flexDirection: 'column'}}>
-                <FontAwesome name="circle-thin" size={390} color="indigo" />
+        <PagerView style={{ flex: 1 }} initialPage={0}>
+        
+            <View key="1">
+            <View style={{ marginTop: 75, alignItems: 'center' }}>
+            <MaterialCommunityIcons name="account-multiple-check-outline" size={50} color="black" />
+            <Text style = {{color: 'indigo'}}>_____________________________________________________</Text>
+        </View>
 
-                </View>
+                    <Text style={{ color: 'black', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 , marginTop: 75}}>The current date for Challenge 25 is:</Text>
 
-                <View style={{ marginTop: 50,}}>
-                    <Text style={{ color: 'black', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>Swipe right for Challenge 18</Text>
 
-                    <View style = {{marginTop:20, marginLeft: 84}}>
-                    <Ionicons name="arrow-forward-circle-outline" size={78} color="indigo" />
+                    <View style={{ marginTop: 150, alignItems: 'center', flexDirection: 'row', gap: 15, justifyContent: 'center', marginBottom: 50 }}>
+                        <Fontisto name="date" size={40} color="indigo" style={{ alignContent: 'flex-start', alignItems: 'flex-start', justifyContent: 'flex-start' }} />
+                        <Text style={{ color: 'black', fontSize: 30 }}> {fullDate} </Text>
+                    </View>
+
+                    <View style={{ marginTop: 110, alignItems: 'center', flexDirection: 'column' }}>
+                        <Text style={{ color: 'black', textAlign: 'center', alignItems: 'center', fontWeight: 'bold', fontSize: 18 }}>Swipe right for Challenge 18</Text>
+                        <View style={{ marginTop: 20 }}>
+                            <Ionicons name="arrow-forward-circle-outline" size={78} color="indigo" />
+                        </View>
+
                     </View>
 
                 </View>
 
-            </View>
+            </PagerView>
         </View>
-
-    )
-}
+    )}
 
 
 export default Challenge25
